95b020a29b6bdba3d065b6c4b5fc319e712c2889
Igor Tuag <igortuag@gmail.com>
2020-06-06 21:45:02 -0300

    Metodos HTTP

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,39 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello Word!' });
+app.get('/projetcs', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+    'Projeto 4',
+  ]);
 });
+
+app.post('/projects', (resquest, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+    'Projeto 4',
+  ]);
+})
+
+app.put('/projects/:id', (resquest, response) => {
+  return response.json([
+    'Projeto 5',
+    'Projeto 6',
+    'Projeto 3',
+    'Projeto 4',
+  ]);
+})
+
+app.put('/projects/:id', (resquest, response) => {
+  return response.json([
+    'Projeto 3',
+    'Projeto 4',
+  ]);
+})
 
 app.listen(3333, () => {
   console.log('🚀 Back-end started on port 3333!')
