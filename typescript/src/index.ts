b5e0a8853d637115c795c66b061bf76cd07da732
Igor Tuag <igortuag@gmail.com>
2020-06-11 08:22:20 -0300

    Configurando o projeto com TS

---
+++ typescript/src/index.ts
@@ -0,0 +1,11 @@
+import express, { response } from 'express';
+
+const app = express();
+
+app.listen(3333);
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
+})
+
+app.listen(3333);
